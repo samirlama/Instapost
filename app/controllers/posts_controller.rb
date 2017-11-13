a5a0ed1b0aa8bb8ffdf9c8ae9bdd47451aa2b0c8
samirlama <samirlama509@gmail.com>
2017-11-13 22:01:11 +0545

    Create operation performed

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,11 @@
 class PostsController < ApplicationController
+	def create
+		@posts = Post.create(post_params)
+		if @posts.valid?
+			redirect_to root_path
+	end
+	private
+	def post_params
+		params.require(:post).permit(:photo,:description,:user_id)
+	end	
 end
