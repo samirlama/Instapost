b7daa840fe80b92ef5a351626f454f5583e3cef0
samirlama <samirlama509@gmail.com>
2017-11-24 13:49:50 +0545

    Edit setting added and all in navbar not in application.htnl.erb

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,9 +3,10 @@ class PostsController < ApplicationController
 		@posts = Post.create(post_params)
 		if @posts.valid?
 			redirect_to root_path
+		end	
 	end
 	private
 	def post_params
-		params.require(:post).permit(:photo,:description,:user_id)
+		params.require(:post).permit(:photo,:description)
 	end	
 end
