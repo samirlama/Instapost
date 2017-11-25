3b0e2e7bfd816f52411c26302d7b8f1ef1299f2f
samirlama <samirlama509@gmail.com>
2017-11-25 22:06:36 +0545

    Create posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,15 +1,20 @@
 class PostsController < ApplicationController
+	before_action :authenticate_user!, only:[new:,:create] 
 	def create
-		@posts = Post.create(post_params)
-		if @posts.valid?
-			redirect_to root_path
-		end	
-	end
-	def new
-		@posts = Post.new
-	end
-	private
-	def post_params
-		params.require(:post).permit(:photo,:description)
+			@posts = current_user.posts.create(posts_params)
+			if @posts.valid?
+				redirect_to root_path
+			else
+				render :new,status: :unprocessable_entity
+			end		
 	end	
+def new 
+@posts = Post.new
+end	
+
+private
+
+def post_params
+  params.require(:post).permit(:user_id, :photo, :description)
+end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,5 @@
 class Post < ApplicationRecord
 	mount_uploader :photo, PhotoUploader
+	belongs_to :user
+	validates :user_id , :photo , :description, presence: true
 end
