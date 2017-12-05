4a53f211ae72f1d9d4346b9f7e0aed5e19241e28
samirlama <samirlama509@gmail.com>
2017-12-05 23:01:49 +0545

    adding validations for comments and creating comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,15 @@
 class CommentsController < ApplicationController
+	before_action :authenticate_user!, only: :create
+	def create
+		@posts = Post.find(params[:post_id])
+		@comments = @posts.comments.create(comment_params.merge(:user_id current_user.id))
+		if @comments.valid?
+			redirect_to root_path
+		end	
+	end	
+	private
+	def comment_params
+		params.require(:comment).permit(:text,:post_id)
+	end	
+
 end
